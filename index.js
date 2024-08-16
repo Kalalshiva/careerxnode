e6f80571107c4df1e313a7c6b61532d2b7688cd5
Kalalshiva <gkalalshiva@gmail.com>
2024-08-16 21:35:15 +0530

    added up to index5.js

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 import express from "express";
 
-var app  = express();
+var app = express();
 
-app.listen(8080,() => {
-    console.log("Server Started on Port 8080");
+app.listen(8080, () => {
+  console.log("Server Started on Port 8080");
 });
 
-app.get("/", (req,res) => {
-    res.send("GET request for /");
+
+app.get("/", (req, res) => {
+  res.send("GET request for /");
 });
 
-app.get("/Shiva",(req,res) => {
-    res.send("Hello Shiva");
+
+app.get("/users", (req, res) => {
+  res.send("Got a GET request for /users");
+});
+
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get("/ab*cd", (req, res) => {
+  res.send("Got a GET request for /ab*cd");
+});import express from "express";
+
+var app = express();
+
+app.listen(8080, () => {
+  console.log("Server Started on Port 8080");
+});
+
+
+app.get("/", (req, res) => {
+  res.send("GET request for /");
+});
+
+
+app.get("/users", (req, res) => {
+  res.send("Got a GET request for /users");
+});
+
+
+
+app.get("/ab*cd", (req, res) => {
+  res.send("Got a GET request for /ab*cd");
 });
